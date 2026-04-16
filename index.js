b88c162a12d0d71b4d4892f288bd26abe2f54f7d
Rox Sabil <roxsabil25@gmail.com>
2026-04-17 02:10:58 +0600

    index update

--- index.js
+++ index.js
@@ -125,6 +125,10 @@ app.get('/projectshocase/:title',async (req,res)=>{
     res.render('projectshocase', { projects })
 });
 
+// Health Check Route
+app.get('/healthz', (req, res) => {
+    res.status(200).send('Server is healthy and running!');
+});
 
 // Server
 
